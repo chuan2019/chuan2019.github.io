92835d28a9713d5365f9894e3b6057333638ae2b
Chuan Zhang <chuan.zhang2015@gmail.com>
2026-06-28 14:29:59 -0700

    build: update site 2026-06-28 14:29

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1782682152',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1782682199',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
